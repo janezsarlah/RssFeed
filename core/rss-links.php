b6e88c598d908dc967dae8aa861c20abc81711dc
Janez Šarlah <janez.sarlah@gmail.com>
2015-05-06 15:59:14 +0200

    Modified rss-links.php. It is now posible to add rss links. Modified rss.php. It crudely displays all the feeds.

---
+++ core/rss-links.php
@@ -0,0 +1,11 @@
+<?php 
+
+/*
+ Used php 5.3 array declaration because of server limitations
+*/
+$links = array(
+	'http://www.24ur.com/rss',
+	'http://www.racunalniske-novice.com/rss-novice/',
+	'http://feeds.feedburner.com/DeveloperTea'
+);
+
--- core/rss.php
+++ core/rss.php
@@ -2,20 +2,20 @@
 
 ini_set('display_errors',1);  
 error_reporting(E_ALL);
+	
+include 'rss-links.php';
 
-function getFeed() {
-	$xml = 'http://www.24ur.com/rss';
-
-	$channel = file_get_contents($xml);
+foreach ($links as $link) {
+	$channel = file_get_contents($link);
 
 	$content = new SimpleXmlElement($channel);
 
 	echo "<ul>";
-     
-    foreach($content->channel->item as $item) {
-        echo "<li><a href = '$item->link' title = '$item->title' target = '_blank'>" . $item->title . "</a></li>";
-    }
-    echo "</ul>";
+	 
+	foreach($content->channel->item as $item) {
+	    echo "<li><a href = '$item->link' title = '$item->title' target = '_blank'>" . $item->title . "</a></li>";
+	}
+	echo "</ul>";
 }
 
-getFeed();
+
