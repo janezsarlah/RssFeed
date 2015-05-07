2d581db04b790a91d26409add3df47d3fb3f429b
Janez Šarlah <janez.sarlah@gmail.com>
2015-05-07 17:46:15 +0200

    Add header and footer. Green color scheme

--- core/RssLinks.php
+++ core/RssLinks.php
@@ -4,13 +4,13 @@
  Used php 5.3 array declaration because of server limitations
 */
 $urls = array(
-	'http://www.24ur.com/rss',
-	'http://www.racunalniske-novice.com/rss-novice/',
+	//'http://www.24ur.com/rss',
 	'http://feeds.feedburner.com/DeveloperTea',
-	'http://www.itnews.com/feed',
-	'http://www.friko.si/index.php/rss/xml/RSS2.0/full',
 	'http://www.sitepoint.com/feed/',
 	'http://www.computerweekly.com/rss/All-Computer-Weekly-content.xml',
+	'http://www.itnews.com/feed',
+	'http://www.racunalniske-novice.com/rss-novice/',
+	'http://www.friko.si/index.php/rss/xml/RSS2.0/full',
 	//'https://news.ycombinator.com/rss'
 );
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype hrml>
 <html lang="en">
 <head>
-	<title>Rss Feeds</title>
+	<title>Latest Blog Posts</title>
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,8 +11,33 @@
 
 </head>
 <body>
+	<div class="navbar navbar-default" role="navigation">
+		<div class="mod-container container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span>
+				</button>
+
+				<a style="font-weight: 600" class="navbar-brand" href="/">Latest Blog Posts</a>
+			</div>
+		</div>
+	</div>
+
 	<div class="container">
 		<?php include_once 'core/RssFeeds.php'; ?>
 	</div>
+
+	<footer class="footer">
+      	<div class="container">
+        	<div class="col-md-12">
+        		<p>Source code <a href="https://github.com/janezsarlah/RssFeed" target="_blank">RssFeed</a></p>
+				<p><h6>Created by <a href="https://twitter.com/janezsarlah" target="_blank">@janezsarlah</a></h6></p>
+			</div>
+      	</div>
+    </footer>
 </body>
 </html>
